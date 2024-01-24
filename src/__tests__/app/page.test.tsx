113000f52a13b26359bf9a54e1ca9a6918018e70
shota <xyz.shotao@gmail.com>
2024-01-24 17:57:20 +0900

    build: testing framework was changed to vitest

--- src/__tests__/app/page.test.tsx
+++ src/__tests__/app/page.test.tsx
@@ -1,11 +1,10 @@
 import Home from '@/app/page';
 import { render, screen } from '@testing-library/react';
+import { expect, test } from 'vitest';
 
-describe('Home', () => {
-  it('renders heading', () => {
-    render(<Home />);
+test('renders heading', () => {
+  render(<Home />);
 
-    const heading = screen.getByRole('heading', { level: 1 });
-    expect(heading).toHaveTextContent('Home');
-  });
+  const heading = screen.getByRole('heading', { level: 1 });
+  expect(heading.textContent).toBe('Home');
 });
