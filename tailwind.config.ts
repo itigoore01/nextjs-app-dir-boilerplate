240f374b62562b5966ee204e4199d05fce301200
itigoore01 <xyz.shotao@gmail.com>
2024-01-24 17:35:31 +0900

    refactor: updated to match the latest create-next-app (#449)

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import { fontFamily } from 'tailwindcss/defaultTheme';
+import type { Config } from 'tailwindcss';
+
+export default {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-noto-sans-jp)', ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
