140d0d5b9f34c3aa3176eb2deefafb365159ca9e
shota <xyz.shotao@gmail.com>
2024-02-16 12:03:03 +0900

    test: enable globals options (#506)

--- src/__tests__/app/page.test.tsx
+++ src/__tests__/app/page.test.tsx
@@ -1,6 +1,5 @@
 import Home from '@/app/page';
 import { render, screen } from '@testing-library/react';
-import { expect, test } from 'vitest';
 
 test('renders heading', () => {
   render(<Home />);
